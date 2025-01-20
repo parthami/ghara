e6f0915800aae5b400c6ef8eefca35f64f46ccf8
Parth Chandratreya <parth.chandratreya@pod-point.com>
2025-01-20 21:41:08 +0000

    add panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css'
-
-function App() {
+const App = () => {
   const time = new Date().toISOString()
 
   return (
-    <div className="flex h-screen">
-        <span className="m-auto">{time}</span>
+    <div>
+        <div className="h-screen grid grid-cols-2 grid-rows-2 gap-4">
+            <div className="bg-gray-200 flex">
+                <span className="m-auto">{time}</span>
+            </div>
+            <div className="bg-gray-300 flex">
+                <span className="m-auto">Panel 2</span>
+            </div>
+            <div className="bg-gray-300 flex">
+                <span className="m-auto">Panel 3</span>
+            </div>
+            <div className="bg-gray-200 flex">
+                <span className="m-auto">Panel 4</span>
+            </div>
+        </div>
     </div>
   )
-}
+};
 
 export default App
