45987ffc6b8ee3fa10feb5962c04604fe3dae6e5
Parth Chandratreya <parth.chandratreya@pod-point.com>
2025-02-28 15:44:06 +0000

    add weather in 4 hours time

--- website/App.tsx
+++ website/App.tsx
@@ -1,25 +1,26 @@
-import {getWeather} from "./panels/weather.ts";
+import { getWeatherPanelData } from "./panels/weather.ts";
 import {useEffect, useState} from "react";
 
 const App = () => {
   const currentTime = new Date();
   const time = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
-  const [weatherText, setWeatherText] = useState("");
+  const [weatherPanelData, setWeatherPanelData] = useState({today: '', inFourHours: ''});
 
   useEffect(() => {
-      getWeather().then(weather =>
-      setWeatherText(weather.daily.weatherDescription.description)
+      getWeatherPanelData().then(weatherPanelData =>
+      setWeatherPanelData(weatherPanelData)
       )
   }, []);
 
   return (
     <div>
-        <div className="h-screen grid grid-cols-2 grid-rows-2 gap-4">
+        <div className="h-[480px] w-[800px] grid grid-cols-2 grid-rows-2 gap-4">
             <div className="bg-gray-200 flex">
                 <span className="m-auto text-9xl">{time}</span>
             </div>
-            <div className="bg-gray-300 flex">
-                <span className="m-auto text-6xl">{weatherText}</span>
+            <div className="bg-gray-300 flex flex-col">
+                <span className="m-auto text-4xl">Today: {weatherPanelData.today}</span>
+                <span className="m-auto text-4xl">In 4 hours: {weatherPanelData.inFourHours}</span>
             </div>
             <div className="bg-gray-300 flex">
                 <span className="m-auto">Panel 3</span>
