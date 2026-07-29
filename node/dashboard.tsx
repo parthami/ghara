60397ee235e4aeb6dd0fa66468852360e90271c8
parthami <chandratreya.parth@gmail.com>
2026-07-29 21:24:53 +0100

    add time to dash

--- node/dashboard.tsx
+++ node/dashboard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';                                                                                          
                                                                                                                     
-const Dashboard: React.FC = () => {                                                                                
+const Dashboard: React.FC = () => {
+  const now = Temporal.Now.zonedDateTimeISO().toString();
+                                                                                  
   return (                                                                                                          
-    <div style={{ backgroundColor: 'white', display: 'flex', height: '100%', width: '100%' }}>                      
-      <div style={{ color: 'black' }}>hello, world</div>                                                            
+    <div style={{ backgroundColor: 'white', display: 'flex', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center' }}>                      
+      <div style={{ color: 'black' }}>hello, world {now}</div>                                                            
     </div>                                                                                                          
   );                                                                                                                
 };                                                                                                                  
