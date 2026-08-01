d3ab9e09a1e32e8c14f32c03f807f2d72e1b3771
parthami <chandratreya.parth@gmail.com>
2026-08-01 10:40:20 +0100

    rework cron

--- node/ghara.ts
+++ node/ghara.ts
@@ -1,3 +1,12 @@
  import {generateImage} from "./generateImage"
 
+Bun.cron("5 * * * *",
+  async () => {
+    console.log("[CRON] Starting generate image job...");
+    await generateImage()
+  },
+);
+
+console.log("Ghara started with active Bun cron schedule.");
+
 generateImage()
