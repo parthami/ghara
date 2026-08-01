7f9713fc72745d1bcbd51f99879e072080108dc6
parthami <chandratreya.parth@gmail.com>
2026-08-01 10:43:43 +0100

    service updates

--- systemd/setup.sh
+++ systemd/setup.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# Copy unit files to system directory
 sudo cp ghara.service /etc/systemd/system/
-sudo cp ghara.timer /etc/systemd/system/
 
 sudo chmod 644 /etc/systemd/system/ghara.service
-sudo chmod 644 /etc/systemd/system/ghara.timer
 
-# Reload systemd to pick up new changes
 sudo systemctl daemon-reload
 
-# Enable and start the timer
-sudo systemctl enable --now ghara.timer
-
-echo "systemd service and timer installed and started successfully!"
+echo "systemd service installed and started successfully!"
