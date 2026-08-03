d7a86d65d01bc92a7b750129f9825de178d4eff6
parthami <chandratreya.parth@gmail.com>
2026-08-03 18:27:14 +0100

    add biome

--- node/dashboard.tsx
+++ node/dashboard.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import {type WeatherHourlyData} from'./weather';
+import type React from "react";
+import type { WeatherHourlyData } from "./weather";
 
 interface DashboardProps {
-  weatherData: WeatherHourlyData[],
+	weatherData: WeatherHourlyData[];
 }
-                                                                                                                    
-const Dashboard: React.FC<DashboardProps> = ({ weatherData }) => {
 
-  return (                                                                                                          
-    <div style={{ backgroundColor: 'white', display: 'flex', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center' }}>                      
-      <div style={{ color: 'black',  display: 'flex' }}>{`hello, world!! ${weatherData.map((data) => `(${data.hour} - ${data.temperature.toFixed(1)})`).join(', ')}`}</div>                                                            
-    </div>                                                                                                          
-  );                                                                                                                
-};                                                                                                                  
-                                                                                                                    
-export default Dashboard;  
+const Dashboard: React.FC<DashboardProps> = ({ weatherData }) => {
+	return (
+		<div
+			style={{
+				backgroundColor: "white",
+				display: "flex",
+				height: "100%",
+				width: "100%",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<div
+				style={{ color: "black", display: "flex" }}
+			>{`hello, world!! ${weatherData.map((data) => `(${data.hour} - ${data.temperature.toFixed(1)})`).join(", ")}`}</div>
+		</div>
+	);
+};
+
+export default Dashboard;
--- node/ghara.ts
+++ node/ghara.ts
@@ -1,14 +1,12 @@
- import {generateImage} from "./generateImage"
+import { generateImage } from "./generateImage";
 
- if(process.env.ENVIRONMENT !== "DEV") {
-Bun.cron("5 * * * *",
-  async () => {
-    console.log("[CRON] Starting generate image job...");
-    await generateImage()
-  },
-);
- }
+if (process.env.ENVIRONMENT !== "DEV") {
+	Bun.cron("5 * * * *", async () => {
+		console.log("[CRON] Starting generate image job...");
+		await generateImage();
+	});
+}
 
 console.log("Ghara started with active Bun cron schedule.");
 
-generateImage()
+generateImage();
