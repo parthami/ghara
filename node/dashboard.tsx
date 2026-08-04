3bad1eda01ceb4a7417cf64909ef07e1cc001075
parthami <chandratreya.parth@gmail.com>
2026-08-04 16:36:50 +0100

    add background and placeholder layout

--- node/dashboard.tsx
+++ node/dashboard.tsx
@@ -8,16 +8,19 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = ({ weatherData }) => {
 	const formattedWeather = (weather: WeatherHourlyData) => {
 		return (
-			<div tw="flex flex-col mr-8">
-				<div tw="flex mb-4 text-4xl">{weather.temperature.toFixed(1)}°C</div>
-				<div tw="flex text-3xl">{weather.hour}:00</div>
+			<div tw="flex flex-col w-1/4 items-center justify-center text-center" key={weather.hour}>
+				<div tw="flex text-3xl">{weather.hour}</div>
+				<div tw="flex my-8">(icon)</div>
+				<div tw="flex text-4xl font-bold">{Math.round(weather.temperature)}°</div>
 			</div>
 		);
 	};
 
 	return (
 		<div tw="flex h-full w-full items-center justify-center bg-white">
+			<div tw="flex w-3/4 items-center justify-center bg-gray-200 p-16 rounded-32">
 			{weatherData.map((weather, index) => formattedWeather(weather))}
+			</div>
 		</div>
 	);
 };
